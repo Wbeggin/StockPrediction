53bba21b861e06733866382ae2e27211de768500
Montonen Weerti Paavo Mikael <weerti.p.montonen@student.jyu.fi>
2024-01-15 00:59:46 +0200

    Add project files.

---
+++ Models/StockData.cs
@@ -0,0 +1,10 @@
+﻿namespace StockPredictionWeb.Models
+{
+    public class StockData
+    {
+        public DateTime Date { get; set; }
+        public double ActualPrices { get; set; }
+        public double PredictedPrices { get; set; }
+    }
+
+}
---
+++ Service/StockDataService.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using StockPredictionWeb.Models;
+using System.Globalization;
+
+
+public class StockDataService
+{
+    private readonly HttpClient httpClient;
+
+    public StockDataService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task<List<StockData>> GetStockDataAsync()
+    {
+        var csvContent = await httpClient.GetStringAsync("data/stock_prediction_data.csv");
+
+        using (var reader = new StringReader(csvContent))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            return csv.GetRecords<StockData>().ToList();
+        }
+    }
+}
